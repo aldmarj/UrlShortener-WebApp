a45accec459dd991676242b93ad077a6435cbdf2
Ben Haynes <benhaynesse@gmail.com>
2018-04-30 18:06:36 +0100

    Testing push

---
+++ shortener/src/components/FormComponents/InputValidation.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class InputValidation extends Component {
+  state = {
+    error: ''
+  };
+
+  render() {
+    return <div>Input</div>;
+  }
+}
